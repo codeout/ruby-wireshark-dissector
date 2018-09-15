fbdac83036eeeee3781cb0142be248268e7ec2d6
Shintaro Kojima <goodies@codeout.net>
2018-09-16 08:35:40 +0900

    Initial implementation

--- extconf.rb
+++ extconf.rb
@@ -1,3 +1,16 @@
 require 'mkmf'
 
+WIRESHARK_VERSION = '2.4.5'
+
+# $CFLAGS += ' -g -O3'
+$CFLAGS += ' ' + `pkg-config --cflags glib-2.0`.strip
+$CFLAGS += " -I./wireshark-#{WIRESHARK_VERSION}"
+$LDFLAGS += ' -lwireshark -lwiretap -lwsutil -lglib-2.0'
+
+create_header('config.h')
+File.open('config.h', 'ab') do |f|
+  f.puts "#define WS_NORETURN __attribute((noreturn))"
+  f.puts "#define _U_ __attribute__((unused))"
+end
+
 create_makefile('wireshark')
--- wireshark_rb.c
+++ wireshark_rb.c
@@ -1,19 +1,44 @@
 #include "ruby.h"
 
-void rawshark_process_packet();
+void rawshark_init();
+void rawshark_clean();
+void rawshark_process_packet(uint8_t *data, int len, FILE *file);
 
-VALUE wrap_rawshark_process_packet(self, data) VALUE self, data;
-{
-    uint8_t *raw;
-
-    raw = (uint8_t *)StringValuePtr(data);
-    rawshark_process_packet(raw);
+VALUE rb_rawshark_init() {
+    rawshark_init();
     return Qnil;
 }
 
-void Init_wireshark() {
-    VALUE module;
+VALUE rb_rawshark_clean() {
+    rawshark_clean();
+    return Qnil;
+}
 
-    module = rb_define_module("Wireshark");
-    rb_define_module_function(module, "dissect", wrap_rawshark_process_packet, 1);
+VALUE rb_rawshark_process_packet(VALUE self, VALUE data) {
+    uint8_t *raw = (uint8_t *) StringValuePtr(data);
+    int rawlen = (int) RSTRING_LEN(data);
+    VALUE json;
+
+    char *buf = NULL;
+    size_t buflen = 0;
+    FILE *out = open_memstream(&buf, &buflen);
+
+    rawshark_process_packet(raw, rawlen, out);
+    fclose(out);
+
+    if (buflen > 4) {
+        // NOTE: Truncate leading "  ,\n"
+        json = rb_str_new2(buf + 4 * sizeof(char));
+    }
+
+    free(buf);
+    return json;
+}
+
+void Init_wireshark() {
+    VALUE module = rb_define_module("Wireshark");
+
+    rb_define_module_function(module, "load", rb_rawshark_init, 0);
+    rb_define_module_function(module, "unload", rb_rawshark_clean, 0);
+    rb_define_module_function(module, "dissect", rb_rawshark_process_packet, 1);
 }
